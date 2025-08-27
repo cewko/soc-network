970cc583755f32606a7ebdb4073705866310ee6b
cewko <221901489+cewko@users.noreply.github.com>
2025-08-27 21:16:03 +0200

    authentication using google

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+SOCIAL_AUTH_GOOGLE_OAUTH_KEY=config("GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH_SECRET=config("GOOGLE_OAUTH2_SECRET")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = "django-insecure-!#)4dp-4$f8#23qs7zgvrmz$8x(3%92k67msvp*wcr_9e8)p+q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -39,6 +43,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "social_django",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -136,4 +143,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "account.authentication.EmailAuthBackend",
+    "social_core.backends.google.GoogleOAuth2",
 ]
