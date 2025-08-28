b18c632088a8ce6269c4f9efe9a64a26501afb24
cewko <221901489+cewko@users.noreply.github.com>
2025-08-28 14:57:05 +0200

    many-to-many relationship of image likes and users

--- images/models.py
+++ images/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 
 class Image(models.Model):
     user = models.ForeignKey(
-        settings.AUTH_USER_USER,
+        settings.AUTH_USER_MODEL,
         related_name="images_created",
         on_delete=models.CASCADE
     )
@@ -15,10 +15,15 @@ class Image(models.Model):
     image = models.ImageField(upload_to="images/%Y/%m/%d")
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="images_liked",
+        blank=True
+    )
 
     class Meta:
         indexes = [models.Index(fields=["-created"]),]
-        ordering = "-created"
+        ordering = ["-created"]
 
     def __str__(self):
         return self.title
