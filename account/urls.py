6586477153a5bf37d495b448e5d5eee4072e538e
cewko <221901489+cewko@users.noreply.github.com>
2025-09-03 17:16:25 +0200

    remove unused import from account urls

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
+
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
